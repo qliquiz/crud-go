4a74e12c23f32aa65d4ac3d02d0819b62fc75d0f
qliquiz <artgor005@gmail.com>
2024-08-28 13:38:02 +0800

    db

--- internal/app/store/store.go
+++ internal/app/store/store.go
@@ -8,6 +8,7 @@ import (
 type Store struct {
 	config *Config
 	db *sql.DB
+	userRepository *UserRepository
 }
 
 func New(config *Config) *Store {
@@ -32,5 +33,17 @@ func (s *Store) Open() error {
 }
 
 func (s *Store) Close() {
+	s.db.Close()
+}
 
+func (s *Store) User() *UserRepository {
+	if s.userRepository != nil {
+		return s.userRepository
+	}
+
+	s.userRepository = &UserRepository{
+		store: s,
+	}
+
+	return s.userRepository
 }
